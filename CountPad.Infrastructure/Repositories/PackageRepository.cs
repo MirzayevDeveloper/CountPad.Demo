69e029e9464995f4178da30528d7becc1840e075
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-04-07 01:14:29 +0500

    REPOSITORY: Add Package

--- CountPad.Infrastructure/Repositories/PackageRepository.cs
+++ CountPad.Infrastructure/Repositories/PackageRepository.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CountPad Team
+// --------------------------------------------------------
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
