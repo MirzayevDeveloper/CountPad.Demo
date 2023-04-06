912c9cbcc552670a50c73296a9779b1616bb6c56
Abror Nabiddinov <nabiddinovabror8@gmail.com>
2023-04-06 10:04:07 +0500

    DATA: Create Model User

--- CountPad.Domain/Models/User.cs
+++ CountPad.Domain/Models/User.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CountPad.Domain.Model
+﻿namespace CountPad.Domain.Model
 {
     internal class User
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
     }
 }
