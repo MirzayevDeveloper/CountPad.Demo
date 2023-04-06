bdbad3bf59c253927a08df972cbb88885677d3bf
MirzayevDeveloper <MirzayevDeveloper1@gmail.com>
2023-04-06 23:06:48 +0500

    DATA: Create Order & Sold

--- CountPad.Domain/Models/Orders/Order.cs
+++ CountPad.Domain/Models/Orders/Order.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CountPad.Domain.Models.Packages;
+using CountPad.Domain.Models.Solds;
 
 namespace CountPad.Domain.Models.Orders
 {
     public class Order
     {
         public Guid Id { get; set; }
+        public Package Package { get; set; }
+        public Sold Sold { get; set; }
     }
 }
