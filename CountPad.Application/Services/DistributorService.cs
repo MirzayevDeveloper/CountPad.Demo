eaa60d218207a122fe2aaf49f37305a6d9893a09
Xurshid-A-J <abdxurshid02@gmail.com>
2023-04-07 10:53:32 +0500

    REPOSITORY : Add Distributor Completed

---
+++ CountPad.Application/Services/DistributorService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using CountPad.Application.Interfaces.RepositoryInterfaces;
+using CountPad.Application.Interfaces.ServiceInterfaces;
+using CountPad.Domain.Models.Distributors;
+
+namespace CountPad.Application.Services
+{
+    public class DistributorService : IDistributorService
+    {
+        private readonly IDistributorRepository distributorRepository;
+
+        public DistributorService(IDistributorRepository distributorRepository) =>
+            this.distributorRepository = distributorRepository;
+
+        public async Task<int> AddDistributorAsync(Distributor distributor)
+        {
+            return await this.distributorRepository.AddAsync(distributor);
+        }
+    }
+}
+
--- CountPad.Infrastructure/Repositories/DistributorRepository.cs
+++ CountPad.Infrastructure/Repositories/DistributorRepository.cs
@@ -1,16 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using CountPad.Application.Interfaces.RepositoryInterfaces;
 using CountPad.Domain.Models.Distributors;
+using CountPad.Domain.Models.Products;
+using Dapper;
+using Npgsql;
 
 namespace CountPad.Infrastructure.Repositories
 {
     public class DistributorRepository : BaseRepository, IDistributorRepository
     {
 
-        public Task<int> AddAsync(Distributor entity)
+        public async Task<int> AddAsync(Distributor distributor)
         {
-            throw new NotImplementedException();
+            using (NpgsqlConnection connection = CreateConnection())
+            {
+
+                string sql = @"INSERT INTO Distributors (Id, Name, Description, Product_Type) 
+                                            VALUES (@Id, @Name, @Description, @ProductType)";
+
+                int affectedRows = connection.Execute(sql,
+                    new Dictionary<string, object>
+                    {
+                        { "Id", distributor.Id },
+                        { "Name", distributor.Name },
+                        { "Description", distributor.CompanyPhone },
+                        { "ProductType", distributor.DelivererPhone}
+                    });
+
+                return affectedRows;
+            }
         }
     }
 }
