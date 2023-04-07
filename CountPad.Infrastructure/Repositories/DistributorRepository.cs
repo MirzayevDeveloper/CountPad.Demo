4e568ad571f2745557e857ba898088625e52ac6d
Xurshid-A-J <abdxurshid02@gmail.com>
2023-04-07 10:40:10 +0500

    REPOSITORY: Add Distributor ( WIP )

---
+++ CountPad.Infrastructure/Repositories/DistributorRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using CountPad.Application.Interfaces.RepositoryInterfaces;
+using CountPad.Domain.Models.Distributors;
+
+namespace CountPad.Infrastructure.Repositories
+{
+    public class DistributorRepository : BaseRepository, IDistributorRepository
+    {
+
+        public Task<int> AddAsync(Distributor entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
