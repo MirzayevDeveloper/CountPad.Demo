751b6b3f01e7a0233b49d3d3ba2e9061d11db19e
MirzayevDeveloper <MirzayevDeveloper1@gmail.com>
2023-04-09 18:51:44 +0500

    Add OrderRepository

---
+++ CountPad.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,19 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CountPad Team
+// --------------------------------------------------------
+
+using System.Threading.Tasks;
+using CountPad.Application.Interfaces.RepositoryInterfaces;
+using CountPad.Domain.Models.Orders;
+
+namespace CountPad.Infrastructure.Repositories
+{
+    public class OrderRepository : BaseRepository, IOrderRepository
+    {
+        public Task<int> AddAsync(Order entity)
+        {
+
+        }
+    }
+}
