92e4b850687dac3c968f170259ba45b1f118dc99
MirzayevDeveloper <MirzayevDeveloper1@gmail.com>
2023-04-09 18:53:47 +0500

    Order Repository Add is finished

--- CountPad.Infrastructure/Repositories/OrderRepository.cs
+++ CountPad.Infrastructure/Repositories/OrderRepository.cs
@@ -6,14 +6,33 @@
 using System.Threading.Tasks;
 using CountPad.Application.Interfaces.RepositoryInterfaces;
 using CountPad.Domain.Models.Orders;
+using Dapper;
+using Npgsql;
 
 namespace CountPad.Infrastructure.Repositories
 {
     public class OrderRepository : BaseRepository, IOrderRepository
     {
-        public Task<int> AddAsync(Order entity)
+        public async Task<int> AddAsync(Order entity)
         {
+            using (NpgsqlConnection connection = CreateConnection())
+            {
+                connection.Open();
+                string query = $"INSERT INTO orders " +
+                            $"(id, package_id, sold_id, count, sold_price)" +
+                            "VALUES(@Id, @Package, @Sold, @Count, @SoldPrice)";
 
+                int changedNumber = await connection.ExecuteAsync(query, new
+                {
+                    Id = entity.Id,
+                    Package = entity.Package.Id,
+                    Sold = entity.Sold.Id,
+                    Count = entity.Package.Count,
+                    SoldPrice = entity.Package.SalePrice
+                });
+
+                return changedNumber;
+            }
         }
     }
 }
