cbc3f59da7001d8ff6dfc5bfb290725ea614218b
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-04-09 23:42:47 +0500

    CODE RUB: Change methods in Package

--- CountPad.Application/Services/PackageService.cs
+++ CountPad.Application/Services/PackageService.cs
@@ -3,6 +3,7 @@
 // Developed by CountPad Team
 // --------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using CountPad.Application.Interfaces.RepositoryInterfaces;
@@ -24,14 +25,14 @@ namespace CountPad.Application.Services
         public async Task<int> AddRangePackageAsync(IEnumerable<Package> packages) =>
             await this.packageRepository.AddRangeAsync(packages);
 
-        public async Task<int> DeletePackageAsync(int packageId) =>
+        public async Task<int> DeletePackageAsync(Guid packageId) =>
             await this.DeletePackageAsync(packageId);
 
 
         public async Task<List<Package>> GetAllPackageAsync() =>
             await this.packageRepository.GetAllAsync();
 
-        public async Task<Package> GetPackageByIdAsync(int packageId) =>
+        public async Task<Package> GetPackageByIdAsync(Guid packageId) =>
             await this.packageRepository.GetByIdAsync(packageId);
 
         public async Task<int> UpdatePackageAsync(Package package) =>
