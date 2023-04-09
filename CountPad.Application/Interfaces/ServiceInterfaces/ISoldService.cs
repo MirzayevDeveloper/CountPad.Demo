3fc8931db4a903863f57848d12e60ffcf4018aab
MirzayevDeveloper <MirzayevDeveloper1@gmail.com>
2023-04-09 22:50:39 +0500

    Sold Service finsihed

---
+++ CountPad.Application/Interfaces/ServiceInterfaces/ISoldService.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CountPad Team
+// --------------------------------------------------------
+
+using CountPad.Domain.Models.Orders;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using CountPad.Domain.Models.Solds;
+
+namespace CountPad.Application.Interfaces.ServiceInterfaces
+{
+    public interface ISoldService
+    {
+        Task<int> AddSoldAsync(Sold sold);
+        Task<int> AddRangeSoldAsync(IEnumerable<Sold> solds);
+        Task<Sold> GetSoldByIdAsync(Guid id);
+        Task<List<Sold>> GetAllSoldsAsync();
+        Task<int> UpdateSoldAsync(Sold sold);
+        Task<int> DeleteSoldAsync(Guid id);
+    }
+}
