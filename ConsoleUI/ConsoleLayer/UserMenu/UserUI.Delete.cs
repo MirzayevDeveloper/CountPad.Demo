8ff29fc53cc87bc88412f93dc83821c9cfeaeafc
Abror Nabiddinov <nabiddinovabror8@gmail.com>
2023-04-10 11:05:41 +0500

    UserMenu

--- ConsoleUI/ConsoleLayer/UserMenu/UserUI.Delete.cs
+++ ConsoleUI/ConsoleLayer/UserMenu/UserUI.Delete.cs
@@ -1,7 +1,15 @@
-﻿namespace ConsoleUI.ConsoleLayer.UserMenu
+﻿using CountPad.Domain.Models.Products;
+using CountPad.Domain.Models.Users;
+
+namespace ConsoleUI.ConsoleLayer.UserMenu
 {
     public partial class UserUI
     {
+        private User DeleteUser()
+        {
+            User user = SelectUser().Result ?? new();
 
+            return user;
+        }
     }
 }
--- ConsoleUI/ConsoleLayer/UserMenu/UserUI.Select.cs
+++ ConsoleUI/ConsoleLayer/UserMenu/UserUI.Select.cs
@@ -1,7 +1,31 @@
-﻿namespace ConsoleUI.ConsoleLayer.UserMenu
+﻿using CountPad.Domain.Models.Users;
+using System.Threading.Tasks;
+using System;
+using CountPad.Domain.Models.Products;
+using EKundalik.ConsoleLayer;
+using System.Collections.Generic;
+
+namespace ConsoleUI.ConsoleLayer.UserMenu
 {
     public partial class UserUI
     {
+        public async Task<User> SelectUser()
+        {
+            Console.Write("enter User id: ");
+            string userId = Console.ReadLine();
+            Guid id;
+            Guid.TryParse(userId, out id);
+
+            return await this.userService.GetUserByIdAsync(id);
+        }
+
+        public async void SelectAllUser()
+        {
+            List<User> users =
+                await this.userService.GetAllUsersAsync();
+
+            General.SelectAll(users);
+        }
 
     }
 }
