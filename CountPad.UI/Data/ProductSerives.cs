7b66838d492d7c632a207e4101b40a023c37ee33
MirzayevDeveloper <MirzayevDeveloper1@gmail.com>
2023-04-14 09:49:52 +0500

    Changes

---
+++ CountPad.UI/Data/ProductSerives.cs
@@ -0,0 +1,30 @@
+using CountPad.Application.Interfaces.RepositoryInterfaces;
+using CountPad.Application.Interfaces.ServiceInterfaces;
+using CountPad.Application.Services;
+using CountPad.Domain.Models.Products;
+using CountPad.Infrastructure.Repositories;
+
+namespace CountPad.UI.Data
+{
+    public class ProductServices
+    {
+
+        private readonly IProductRepository productRepository;
+        private readonly IProductService productService;
+
+        public ProductServices()
+        {
+            this.productRepository = new ProductRepository();
+
+            this.productService =
+                new ProductService(productRepository);
+        }
+
+        public Task GetProductsAsync()
+        {
+            var products = this.productService.GetAllProducts().Result.ToArray();
+
+            return Task.FromResult(products);
+        }
+    }
+}
