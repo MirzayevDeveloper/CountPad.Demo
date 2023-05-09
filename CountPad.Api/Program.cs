a6e67858e8b47d5a781081d9a623a8f56a266c28
MirzayevDeveloper <MirzayevDeveloper1@gmail.com>
2023-05-09 16:13:53 +0500

    changes

--- CountPad.Api/Program.cs
+++ CountPad.Api/Program.cs
@@ -4,6 +4,7 @@
 // --------------------------------------------------------
 
 using CountPad.Application;
+using CountPad.Infrastructure;
 
 namespace CountPad.Api
 {
@@ -14,6 +15,7 @@ namespace CountPad.Api
 			var builder = WebApplication.CreateBuilder(args);
 
 			builder.Services.AddApplication();
+			builder.Services.AddInfrastructure(builder.Configuration);
 			builder.Services.AddControllers();
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
