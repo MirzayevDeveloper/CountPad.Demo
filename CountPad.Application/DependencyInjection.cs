a937e2a1dca3deea3fd9d9bf051fd19b50096cf4
MirzayevDeveloper <MirzayevDeveloper1@gmail.com>
2023-05-09 17:06:17 +0500

    changes

--- CountPad.Application/DependencyInjection.cs
+++ CountPad.Application/DependencyInjection.cs
@@ -10,11 +10,6 @@ namespace CountPad.Application
 {
 	public static class DependencyInjection
 	{
-		public static IServiceCollection AddApplication(this IServiceCollection services)
-		{
-			services.AddMediatR(x => x.RegisterServicesFromAssembly(assembly: Assembly.GetExecutingAssembly()));
-
-			return services;
-		}
+		
 	}
 }
