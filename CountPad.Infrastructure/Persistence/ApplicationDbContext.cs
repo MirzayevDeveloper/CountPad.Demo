35e8b668c498cd93a21c6a6ce7532e1c91501f0a
MirzayevDeveloper <MirzayevDeveloper1@gmail.com>
2023-05-09 15:57:49 +0500

    changes

--- CountPad.Infrastructure/Persistence/ApplicationDbContext.cs
+++ CountPad.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CountPad Team
+// --------------------------------------------------------
 
 namespace CountPad.Infrastructure.Persistence
 {
-    internal class ApplicationDbContext
-    {
-    }
+	public class ApplicationDbContext
+	{
+	}
 }
