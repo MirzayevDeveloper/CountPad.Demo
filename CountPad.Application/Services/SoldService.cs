a5a52d9441ae2fe327bfada1c9b96528e38f7027
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-05-09 16:12:42 +0500

    Implemented

--- CountPad.Application/Services/SoldService.cs
+++ CountPad.Application/Services/SoldService.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CountPad.Application.Interfaces.ServiceInterfaces;
+using CountPad.Domain.Models.Solds;
 
 namespace CountPad.Application.Services
 {
-    public class SoldService
+    public class SoldService : ISoldService
     {
+        public ValueTask<Sold> AddSoldAsync(Sold sold)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<Sold> DeleteSoldAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Sold> GetAllSoldsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<Sold> GetSoldByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<Sold> UpdateSoldAsync(Sold sold)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
