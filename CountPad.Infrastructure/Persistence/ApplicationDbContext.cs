c8470ce8121cece624a3111886afbbca473534c3
MirzayevDeveloper <MirzayevDeveloper1@gmail.com>
2023-05-09 16:14:24 +0500

    changes

--- CountPad.Infrastructure/Persistence/ApplicationDbContext.cs
+++ CountPad.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -3,9 +3,6 @@
 // Developed by CountPad Team
 // --------------------------------------------------------
 
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 using CountPad.Application.Abstactions;
 using CountPad.Domain.Models.Distributors;
 using CountPad.Domain.Models.Orders;
@@ -23,18 +20,13 @@ namespace CountPad.Infrastructure.Persistence
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
 			: base(options)
 		{
-
 		}
+
 		public DbSet<Distributor> Distributors { get; set; }
 		public DbSet<Order> Orders { get; set; }
 		public DbSet<Package> Packages { get; set; }
 		public DbSet<Product> Products { get; set; }
 		public DbSet<Sold> Solds { get; set; }
 		public DbSet<User> Users { get; set; }
-
-		public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
-		{
-			throw new NotImplementedException();
-		}
 	}
 }
