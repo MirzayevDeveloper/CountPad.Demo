36c38140e711233dfe424485e6131a3f1ce90e67
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-05-09 18:47:01 +0500

    Saved

--- CountPad.Application/Services/Distributors/DistributorService.cs
+++ CountPad.Application/Services/Distributors/DistributorService.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CountPad Team
+// --------------------------------------------------------
+
+using System;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
 using System.Threading.Tasks;
 using CountPad.Application.Abstactions;
 using CountPad.Application.Interfaces.ServiceInterfaces;
 using CountPad.Domain.Models.Distributors;
-using CountPad.Domain.Models.Products;
-using Microsoft.EntityFrameworkCore;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace CountPad.Application.Services
 {
@@ -17,15 +16,21 @@ namespace CountPad.Application.Services
     {
         private readonly IApplicationDbContext _context;
 
-        public DistributorService(IApplicationDbContext context)
-        {
+        public DistributorService(IApplicationDbContext context)=>
             _context = context;
-        }
-        public async ValueTask<Distributor> AddDistributorAsync(Distributor distributor)
-        {
-            return await _context.AddAsync(distributor);
-        }
 
+        public async ValueTask<Distributor> AddDistributorAsync(Distributor distributor)=>
+             await _context.AddAsync(distributor);
+
+        public IQueryable<Distributor> GetAllDistributors()=>
+             _context.GetAll<Distributor>();
+
+        public async ValueTask<Distributor> GetDistributorByIdAsync(Guid id)=>
+             await _context.GetAsync<Distributor>(id);
+
+        public async ValueTask<Distributor> UpdateDistributorAsync(Distributor distributor)=>
+            await _context.UpdateAsync<Distributor>(distributor);   
+        
         public async ValueTask<Distributor> DeleteDistributorAsync(Guid id)
         {
             Distributor maybeDistributor = await _context.GetAsync<Distributor>(id);
@@ -36,22 +41,6 @@ namespace CountPad.Application.Services
             }
 
             return await _context.DeleteAsync(maybeDistributor);
-        }
-
-        public IQueryable<Distributor> GetAllDistributors()
-        {
-            return _context.GetAll<Distributor>();
-        }
-
-        public async ValueTask<Distributor> GetDistributorByIdAsync(Guid id)
-        {
-            return await _context.GetAsync<Distributor>(id);
-        }
-
-        public async ValueTask<Distributor> UpdateDistributorAsync(Distributor distributor)
-        {
-            return await _context.UpdateAsync<Distributor>(distributor);                                 
-        
         }
     }
 }
--- CountPad.Application/Services/Orders/OrderService.cs
+++ CountPad.Application/Services/Orders/OrderService.cs
@@ -8,7 +8,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using CountPad.Application.Abstactions;
 using CountPad.Application.Interfaces.ServiceInterfaces;
-using CountPad.Domain.Models.Distributors;
 using CountPad.Domain.Models.Orders;
 
 namespace CountPad.Application.Services
@@ -16,16 +15,22 @@ namespace CountPad.Application.Services
 	public class OrderService : IOrderService
 	{
         private readonly IApplicationDbContext _context;
-        public OrderService(IApplicationDbContext context)
-        {
+        public OrderService(IApplicationDbContext context)=>
             _context= context;
-        }
-        public async ValueTask<Order> AddOrderAsync(Order order)
-		{
-			return await _context.AddAsync(order);
-		}
+        
+        public async ValueTask<Order> AddOrderAsync(Order order)=>
+             await _context.AddAsync(order);
 
-		public async ValueTask<Order> DeleteOrderAsync(Guid id)
+		public IQueryable<Order> GetAllOrdersAsync()=>
+			 _context.GetAll<Order>();
+
+		public async ValueTask<Order> GetOrderByIdAsync(Guid id)=>
+            await _context.GetAsync<Order>(id);
+
+		public async ValueTask<Order> UpdateOrderAsync(Order order)=>
+            await _context.UpdateAsync<Order>(order);
+	
+        public async ValueTask<Order> DeleteOrderAsync(Guid id)
 		{
             Order maybeOrder = await _context.GetAsync<Order>(id);
 
@@ -35,21 +40,6 @@ namespace CountPad.Application.Services
             }
 
             return await _context.DeleteAsync(maybeOrder);
-        }
-
-		public IQueryable<Order> GetAllOrdersAsync()
-		{
-			return _context.GetAll<Order>();
-		}
-
-		public async ValueTask<Order> GetOrderByIdAsync(Guid id)
-		{
-            return await _context.GetAsync<Order>(id);
-        }
-
-		public async ValueTask<Order> UpdateOrderAsync(Order order)
-		{
-            return await _context.UpdateAsync<Order>(order);
         }
 	}
 }
